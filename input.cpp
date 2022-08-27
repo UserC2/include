0d5b33f8924b9ad7035f8bbc3fd87baa6d5c1ca6
UserC2 <aj01cars@outlook.com>
2022-08-27 12:18:16 -0400

    Remove accidental semicolon in input.cpp

--- input.cpp
+++ input.cpp
@@ -65,7 +65,7 @@ namespace input
 	/* Get full line of input as a string, no input validation.
 	* Manually call invalidInput() if the user enters invalid input. */
 	template <>
-	std::string getInput<std::string>(std::string_view prompt);
+	std::string getInput<std::string>(std::string_view prompt)
 	{
 		std::cout << prompt;
 		std::string input{};
