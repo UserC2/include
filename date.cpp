c842454db2c0304c313ec7a39d677107f7625425
UserC2 <aj01cars@outlook.com>
2022-10-07 20:22:42 -0400

    date::isOlder() and date::isNewer() allow comparison of different dates

--- date.cpp
+++ date.cpp
@@ -1,5 +1,6 @@
 #include "date.h"
 
+#include <cassert>
 #include <chrono>
 #include <ctime>
 #include <iomanip>
@@ -17,4 +18,24 @@ namespace date
             strftime(buffer, formatLength, "%d/%m/%Y", local);
         return buffer;
     }
+
+	std::string formatYMD(std::string_view date)
+	{
+		assert(date.length() == 10
+			&& "date::formatYMD() called with invalid date");
+		std::string day{ date[0], date[1] };
+		std::string month{ date[3], date[4] };
+		std::string year{ date[6], date[7], date[8], date[9] };
+		return { year + month + day };
+	}
+
+	bool isNewer(std::string_view dateA, std::string_view dateB)
+	{
+		return formatYMD(dateA) > formatYMD(dateB);
+	}
+
+	bool isOlder(std::string_view dateA, std::string_view dateB)
+	{
+		return !isNewer(dateA, dateB);
+	}
 }
